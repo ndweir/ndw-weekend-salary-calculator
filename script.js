7b8446339368a5a3fce2d5c4e30d2635faa3a3a4
nick <ndweir13@gmail.com>
2024-09-07 13:48:03 -0500

    CreatedNewFunctions

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+function formSubmit(event){
+    // take form inputs into variables
+
+    // adds row to table
+
+    // take the form input variables and display in DOM on table
+
+
+}
+
+
+function clearForm(){
+    // clears form inputs after a new employee is submitted
+
+}
+
+
+function updateTotalMonthlySalary(){
+    // updates total monthly salary variable
+
+    // updates DOM with total monthly salary when a new
+    // employee is added or deleted
+    
+
+    //applies 'over-budget class to footer when TMS > 20,000
+}
+
+function deleteRowOnTable(){
+    //on Click, delete entire row
+    
+    // when row is deleted make sure monthly salary is updated
+
+}
