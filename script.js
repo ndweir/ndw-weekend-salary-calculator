853d77072bd2054b19d496f832b6fcd526c3507f
nick <ndweir13@gmail.com>
2024-09-07 14:25:34 -0500

    UpdatedTableStyles

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 
 
+
 function formSubmit(event){
     // take form inputs into variables
+    event.preventDefault();
 
+
+    let firstNameSubmit = document.getElementById('firstName').value;
+    let lastNameSubmit = document.getElementById('lastName').value;
+    let iDsubmit = document.getElementById('iDnum').value;
+    let titleSubmit = document.getElementById('title').value;
+    let annualSalarySubmit = document.getElementById('annualSalary').value;
+
+    // console.log(firstNameSubmit);
+    // console.log(lastNameSubmit);
+    // console.log(iDsubmit);
+    // console.log(titleSubmit);
+    // console.log(annualSalarySubmit);
+
+    // document.getElementById('table').append(firstNameSubmit);
+    // document.getElementById('table').append(lastNameSubmit);
+    // document.getElementById('table').append(iDsubmit);
+    // document.getElementById('table').append(titleSubmit);
+    // document.getElementById('table').append(annualSalarySubmit);
     // adds row to table
 
     // take the form input variables and display in DOM on table
