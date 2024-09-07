622711c6da72c685048c42606f19e0027092cf20
nick <ndweir13@gmail.com>
2024-09-07 17:47:59 -0500

    AddingNewRowCorrectly

--- script.js
+++ script.js
@@ -5,12 +5,27 @@ function formSubmit(event){
     // take form inputs into variables
     event.preventDefault();
 
-
     let firstNameSubmit = document.getElementById('firstName').value;
     let lastNameSubmit = document.getElementById('lastName').value;
     let iDsubmit = document.getElementById('iDnum').value;
     let titleSubmit = document.getElementById('title').value;
     let annualSalarySubmit = document.getElementById('annualSalary').value;
+    
+    
+    let tableBody = document.getElementById('tableBody');
+    console.log(tableBody);
+
+    tableBody.innerHTML += `<tr>
+    <td>${firstNameSubmit}</td>
+    <td>${lastNameSubmit}</td>
+    <td>${iDsubmit}</td>
+    <td>${titleSubmit}</td>
+    <td>${annualSalarySubmit}</td>
+    <td>
+      <button id="deleter" onClick="deleteRowOnTable()">Delete</button>
+    </td>
+  </tr>
+  `
 
     // console.log(firstNameSubmit);
     // console.log(lastNameSubmit);
@@ -18,29 +33,6 @@ function formSubmit(event){
     console.log(titleSubmit);
     console.log(annualSalarySubmit);
 
-    // document.getElementById('table').append(firstNameSubmit);
-    // document.getElementById('table').append(lastNameSubmit);
-    // document.getElementById('table').append(iDsubmit);
-    // document.getElementById('table').append(titleSubmit);
-    // document.getElementById('table').append(annualSalarySubmit);
-    // adds row to table
-
-    let table = document.getElementById('tableBody')
-    let row = table.insertRow(0);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
-    let cell5 = row.insertCell(4);
-    let cell6 = row.insertCell(5);
-
-    // take the form input variables and display in DOM on table
-    cell1.innerHTML += firstNameSubmit;
-    cell2.innerHTML += lastNameSubmit;
-    cell3.innerHTML += iDsubmit;
-    cell4.innerHTML += titleSubmit;
-    cell5.innerHTML += annualSalarySubmit;
-   // cell6.innerHTML += btnChild.appendChild("Delete")
    clearForm(event)
 }
 
